ef38831cde9edd4adcf19f1b8c50413b738af23c
Tsuyoshi Akiyama <ka090824@outlook.com>
2017-10-19 04:55:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string == uppercase) {
     return "YES INDEED!"
-  }else {
+  }else if (string == lowercase) {
     return "I can't hear you!"
+  }else if (string == "I love you, Grandma.") {
+    "I love you, too."
   }
 }
